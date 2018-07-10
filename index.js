beb76f614ef30bde85a8c55af65668ab10805666
mondara1234 <40348446+mondara1234@users.noreply.github.com>
2018-07-10 14:45:57 +0700

    test conponent

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ import { AppRegistry, Dimensions } from 'react-native';
 import { DrawerNavigator } from 'react-navigation';
 import SideMenu from './SideMenu'
 import lns from './LoingScreen';
+import  stacknavv from  './stacknav';
 import showsql from './ShowStudentListActivity';
 const drawernav = DrawerNavigator({
     Item1: {
-        screen: showsql,
+        screen: stacknavv,
 
     }
 
